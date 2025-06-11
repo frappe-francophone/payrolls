2becad985d8998a6e1b0c9b3e0f6066471a0945d
frappe-francophone <frappefrancophone@gmail.com>
2025-06-11 04:09:07 +0100

    Ajout des composantes customs

---
+++ payrolls/gestion_de_paie/doctype/custom_paie_settings/custom_paie_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ir Dernis Mataebeka and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Custom Paie Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
